4325121c1aef67aec76f4151717418a6f0bd6a24
Upendra Singh <118816738+iupendra982@users.noreply.github.com>
2024-05-01 23:04:48 +0530

    #

--- Recursion/basics.java
+++ Recursion/basics.java
@@ -95,6 +95,32 @@ public class basics {
         }
         return isFound;
      }
+
+     //Print x^n
+     public static int power(int x,int n){
+        if(n==0){
+            return 1;
+        }
+        // int xnm1=power(x,n-1);
+        // int xn=x*xnm1;
+        // return xn;
+        return x*power(x,n-1);
+     }
+
+     //Optimized Power method with TC==o(n)
+     public static int optimizedPower(int a,int n){
+        if(n==0){
+            return 1;
+        }
+        int halfPower=optimizedPower(a, n/2);
+        //if n is even
+        int halfPowerSq=halfPower*halfPower;
+        //if n is odd
+        if(n%2!=0){
+            halfPowerSq=a*halfPowerSq;
+        }
+        return halfPowerSq;
+     }
     public static void main(String[] args) {
         printDec(10);
         printInc(1);
@@ -109,5 +135,9 @@ public class basics {
         System.out.println(firrstOccurence(arr1, 5, 0));
 
         System.out.println(lastOccurence(arr1, 5, 0));
+
+        System.out.println(power(2, 10));
+
+        System.out.println(optimizedPower(2, 10));
     }
 }
