d561f5fed87d2fa00aebca719613b12a3df29111
Upendra Singh <118816738+iupendra982@users.noreply.github.com>
2024-05-03 23:35:34 +0530

    #

--- Recursion/Problems.java
+++ Recursion/Problems.java
@@ -13,7 +13,28 @@ public class Problems {
         int totWays=fnm1+fnm2;
         return totWays;
     }
+
+    //Remove duplicates in a String
+    public static void removeDuplicates(String str,int idx,StringBuilder newStr,boolean map[]){
+        if(idx==str.length()){
+            System.out.println(newStr);
+            return;
+        }
+        //kaam
+        char currChar=str.charAt(idx);
+        if(map[currChar-'a']==true){
+            //duplicate
+            removeDuplicates(str, idx+1, newStr, map);
+        }
+        else{
+            map[currChar-'a'] = true;
+            removeDuplicates(str, idx+1, newStr.append(currChar), map);
+        }
+    }
     public static void main(String[] args) {
         System.out.println(tillingProblem(4));
+
+        String str="appnnacollege";
+        removeDuplicates(str, 0, new StringBuilder(""),new boolean[26]);
     }
 }
